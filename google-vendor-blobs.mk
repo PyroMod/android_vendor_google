8e262805e88fd00859c3237d83bf940ef4810961
Derek Sevilla <deryk0917@gmail.com>
2012-08-07 22:19:52 -0700

    Add Google Now

--- google-vendor-blobs.mk
+++ google-vendor-blobs.mk
@@ -38,6 +38,7 @@ PRODUCT_COPY_FILES += \
     vendor/google/proprietary/app/Phonesky.apk:system/app/Phonesky.apk \
     vendor/google/proprietary/app/SetupWizard.apk:system/app/SetupWizard.apk \
     vendor/google/proprietary/app/Talk.apk:system/app/Talk.apk \
+    vendor/google/proprietary/app/Velvet.apk:system/app/Velvet.apk \
     vendor/google/proprietary/app/VoiceSearch.apk:system/app/VoiceSearch.apk \
     vendor/google/proprietary/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
     vendor/google/proprietary/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
@@ -51,11 +52,14 @@ PRODUCT_COPY_FILES += \
     vendor/google/proprietary/lib/libflint_engine_jni_api.so:system/lib/libflint_engine_jni_api.so \
     vendor/google/proprietary/lib/libfrsdk.so:system/lib/libfrsdk.so \
     vendor/google/proprietary/lib/libgcomm_jni.so:system/lib/libgcomm_jni.so \
+    vendor/google/proprietary/lib/libgoogle_recognizer_jni.so:system/lib/libgoogle_recognizer_jni.so \
+    vendor/google/proprietary/lib/libpatts_engine_jni_api.so:system/lib/libpatts_engine_jni_api.so \
     vendor/google/proprietary/lib/libpicowrapper.so:system/lib/libpicowrapper.so \
     vendor/google/proprietary/lib/libspeexwrapper.so:system/lib/libspeexwrapper.so \
     vendor/google/proprietary/lib/libvideochat_jni.so:system/lib/libvideochat_jni.so \
     vendor/google/proprietary/lib/libvideochat_stabilize.so:system/lib/libvideochat_stabilize.so \
     vendor/google/proprietary/lib/libvoicesearch.so:system/lib/libvoicesearch.so \
+    vendor/google/proprietary/lib/libvorbisencoder.so:system/lib/libvorbisencoder.so \
     vendor/google/proprietary/tts/lang_pico/de-DE_gl0_sg.bin:system/tts/lang_pico/de-DE_gl0_sg.bin \
     vendor/google/proprietary/tts/lang_pico/de-DE_ta.bin:system/tts/lang_pico/de-DE_ta.bin \
     vendor/google/proprietary/tts/lang_pico/es-ES_ta.bin:system/tts/lang_pico/es-ES_ta.bin \
@@ -64,6 +68,51 @@ PRODUCT_COPY_FILES += \
     vendor/google/proprietary/tts/lang_pico/fr-FR_ta.bin:system/tts/lang_pico/fr-FR_ta.bin \
     vendor/google/proprietary/tts/lang_pico/it-IT_cm0_sg.bin:system/tts/lang_pico/it-IT_cm0_sg.bin \
     vendor/google/proprietary/tts/lang_pico/it-IT_ta.bin:system/tts/lang_pico/it-IT_ta.bin \
+    vendor/google/proprietary/usr/srec/config/en.us/baseline.par:system/usr/srec/config/en.us/baseline.par \
+    vendor/google/proprietary/usr/srec/config/en.us/baseline8k.par:system/usr/srec/config/en.us/baseline8k.par \
+    vendor/google/proprietary/usr/srec/config/en.us/baseline11k.par:system/usr/srec/config/en.us/baseline11k.par \
+    vendor/google/proprietary/usr/srec/config/en.us/dictionary:system/usr/srec/config/en.us/dictionary \
+    vendor/google/proprietary/usr/srec/config/en.us/dictionary/basic.ok:system/usr/srec/config/en.us/dictionary/basic.ok \
+    vendor/google/proprietary/usr/srec/config/en.us/dictionary/cmu6plus.ok.zip:system/usr/srec/config/en.us/dictionary/cmu6plus.ok.zip \
+    vendor/google/proprietary/usr/srec/config/en.us/dictionary/enroll.ok:system/usr/srec/config/en.us/dictionary/enroll.ok \
+    vendor/google/proprietary/usr/srec/config/en.us/g2p/en-US-ttp.data:system/usr/srec/config/en.us/g2p/en-US-ttp.data \
+    vendor/google/proprietary/usr/srec/config/en.us/grammars/boolean.g2g:system/usr/srec/config/en.us/grammars/boolean.g2g \
+    vendor/google/proprietary/usr/srec/config/en.us/grammars/phone_type_choice.g2g:system/usr/srec/config/en.us/grammars/phone_type_choice.g2g \
+    vendor/google/proprietary/usr/srec/config/en.us/grammars/VoiceDialer.g2g:system/usr/srec/config/en.us/grammars/VoiceDialer.g2g \
+    vendor/google/proprietary/usr/srec/config/en.us/models/generic.swiarb:system/usr/srec/config/en.us/models/generic.swiarb \
+    vendor/google/proprietary/usr/srec/config/en.us/models/generic8.lda:system/usr/srec/config/en.us/models/generic8.lda \
+    vendor/google/proprietary/usr/srec/config/en.us/models/generic8_f.swimdl:system/usr/srec/config/en.us/models/generic8_f.swimdl \
+    vendor/google/proprietary/usr/srec/config/en.us/models/generic8_m.swimdl:system/usr/srec/config/en.us/models/generic8_m.swimdl \
+    vendor/google/proprietary/usr/srec/config/en.us/models/generic11.lda:system/usr/srec/config/en.us/models/generic11.lda \
+    vendor/google/proprietary/usr/srec/config/en.us/models/generic11_f.swimdl:system/usr/srec/config/en.us/models/generic11_f.swimdl \
+    vendor/google/proprietary/usr/srec/config/en.us/models/generic11_m.swimdl:system/usr/srec/config/en.us/models/generic11_m.swimdl \
+    vendor/google/proprietary/usr/srec/en-US/acoustic_model:system/usr/srec/en-US/acoustic_model \
+    vendor/google/proprietary/usr/srec/en-US/c_fst:system/usr/srec/en-US/c_fst \
+    vendor/google/proprietary/usr/srec/en-US/clg:system/usr/srec/en-US/clg \
+    vendor/google/proprietary/usr/srec/en-US/compile_grammar.config:system/usr/srec/en-US/compile_grammar.config \
+    vendor/google/proprietary/usr/srec/en-US/contacts.abnf:system/usr/srec/en-US/contacts.abnf \
+    vendor/google/proprietary/usr/srec/en-US/dict:system/usr/srec/en-US/dict \
+    vendor/google/proprietary/usr/srec/en-US/dictation.config:system/usr/srec/en-US/dictation.config \
+    vendor/google/proprietary/usr/srec/en-US/embed_phone_nn_model:system/usr/srec/en-US/embed_phone_nn_model \
+    vendor/google/proprietary/usr/srec/en-US/embed_phone_nn_state_sym:system/usr/srec/en-US/embed_phone_nn_state_sym \
+    vendor/google/proprietary/usr/srec/en-US/endpointer_dictation.config:system/usr/srec/en-US/endpointer_dictation.config \
+    vendor/google/proprietary/usr/srec/en-US/endpointer_voicesearch.config:system/usr/srec/en-US/endpointer_voicesearch.config \
+    vendor/google/proprietary/usr/srec/en-US/ep_acoustic_model:system/usr/srec/en-US/ep_acoustic_model \
+    vendor/google/proprietary/usr/srec/en-US/g2p_fst:system/usr/srec/en-US/g2p_fst \
+    vendor/google/proprietary/usr/srec/en-US/google_hotword.config:system/usr/srec/en-US/google_hotword.config \
+    vendor/google/proprietary/usr/srec/en-US/google_hotword_clg:system/usr/srec/en-US/google_hotword_clg \
+    vendor/google/proprietary/usr/srec/en-US/google_hotword_logistic:system/usr/srec/en-US/google_hotword_logistic \
+    vendor/google/proprietary/usr/srec/en-US/grammar.config:system/usr/srec/en-US/grammar.config \
+    vendor/google/proprietary/usr/srec/en-US/hmmsyms:system/usr/srec/en-US/hmmsyms \
+    vendor/google/proprietary/usr/srec/en-US/hotword_symbols:system/usr/srec/en-US/hotword_symbols \
+    vendor/google/proprietary/usr/srec/en-US/lintrans_model:system/usr/srec/en-US/lintrans_model \
+    vendor/google/proprietary/usr/srec/en-US/metadata:system/usr/srec/en-US/metadata \
+    vendor/google/proprietary/usr/srec/en-US/normalizer:system/usr/srec/en-US/normalizer \
+    vendor/google/proprietary/usr/srec/en-US/norm_fst:system/usr/srec/en-US/norm_fst \
+    vendor/google/proprietary/usr/srec/en-US/offensive_word_normalizer:system/usr/srec/en-US/offensive_word_normalizer \
+    vendor/google/proprietary/usr/srec/en-US/phonelist:system/usr/srec/en-US/phonelist \
+    vendor/google/proprietary/usr/srec/en-US/rescoring_lm:system/usr/srec/en-US/rescoring_lm \
+    vendor/google/proprietary/usr/srec/en-US/symbols:system/usr/srec/en-US/symbols \
     vendor/google/proprietary/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-r0-ri20.2d_n2:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-r0-ri20.2d_n2 \
     vendor/google/proprietary/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rn7-ri20.2d_n2:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rn7-ri20.2d_n2 \
     vendor/google/proprietary/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rp7-ri20.2d_n2:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rp7-ri20.2d_n2 \
